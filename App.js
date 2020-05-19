cbf2d00be7534f1cb98e90713d44cb2fec7f6b65
Harman Saini <saini4097@saskpolytech.ca>
2020-05-19 00:22:59 +0000

    App.js created online with Bitbucket

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import SignIn from "./CMP/Login";
+import Header from "./CMP/Header";
+import Copyright from './CMP/Footer';
+import {BrowserRouter as Router , Switch , Route} from 'react-router-dom';
+import SignUp from "./CMP/SignUp";
+import ForgetPassword from "./CMP/ForgetPassword";
+
+function App() {
+  return (
+      <Router>
+    <div id="App">
+        <Switch>
+        <Route path="/" exact component={home}/>
+        <Route path="/Login" component={SignIn}/>
+        <Route path="/SignUp" component={SignUp}/>
+        <Route path="/Forget" component={ForgetPassword}/>
+        </Switch>
+    </div>
+      </Router>
+  );
+}
+
+const home = () => (
+    <div id="App">
+        <Header/>
+        <Copyright/>
+    </div>
+)
+
+export default App;
